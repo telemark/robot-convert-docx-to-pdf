beb1e67aa293a643ee695fa6e63358f853e0949f
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-06-07 12:24:07 +0200

    Fixes errors

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   DONE_DIRECTORY_PATH: process.env.DONE_DIRECTORY_PATH || 'test/directories/done',
   ERRORS_DIRECTORY_PATH: process.env.ERRORS_DIRECTORY_PATH || 'test/directories/errors',
   QUEUE_DIRECTORY_PATH: process.env.QUEUE_DIRECTORY_PATH || 'test/directories/queue',
-  TEMP_DIRECTORY_PATH: process.env.TEMP_DIRECTORY_PATH || 'test/directory/temp',
+  TEMP_DIRECTORY_PATH: process.env.TEMP_DIRECTORY_PATH || 'test/directories/temp',
   PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'minelev-robot',
   PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
   PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || 12345
