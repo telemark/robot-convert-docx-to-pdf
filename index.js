7e1935aa07f2836c2d523f4e2329fd6e742a3650
maccyber <jonas.enge@gmail.com>
2017-06-07 11:24:07 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict'
 
 const getNextJobFromQueue = require('./lib/steps/get-next-job-from-queue')
+const getFileData = require('./lib/steps/get-file-data')
 const convertDocxToPdf = require('./lib/steps/convert-docx-to-pdf')
 const logger = require('./lib/logger')
 
 logger('info', ['index', 'start'])
 
 getNextJobFromQueue()
+  .then(getFileData)
   .then(convertDocxToPdf)
   .then(() => {
     logger('info', ['index', 'done'])
