ca81051f3d0e387f09a9fd95aa5324a2a736a0b3
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-06-07 12:14:06 +0200

    Initial commit

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 'use strict'
 
 module.exports = {
-  DISTRIBUTION_DIRECTORY_PATH: process.env.DISTRIBUTION_DIRECTORY_PATH || 'test/directories/distribution',
+  DONE_DIRECTORY_PATH: process.env.DONE_DIRECTORY_PATH || 'test/directories/done',
   ERRORS_DIRECTORY_PATH: process.env.ERRORS_DIRECTORY_PATH || 'test/directories/errors',
   QUEUE_DIRECTORY_PATH: process.env.QUEUE_DIRECTORY_PATH || 'test/directories/queue',
+  TEMP_DIRECTORY_PATH: process.env.TEMP_DIRECTORY_PATH || 'test/directory/temp',
   PAPERTRAIL_HOSTNAME: process.env.PAPERTRAIL_HOSTNAME || 'minelev-robot',
   PAPERTRAIL_HOST: process.env.PAPERTRAIL_HOST || 'logs.papertrailapp.com',
   PAPERTRAIL_PORT: process.env.PAPERTRAIL_PORT || 12345
--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const getNextJobFromQueue = require('./lib/steps/get-next-job-from-queue')
 const getFileData = require('./lib/steps/get-file-data')
 const convertDocxToPdf = require('./lib/steps/convert-docx-to-pdf')
+const saveToDone = require('./lib/steps/save-to-done')
+const removeFromQueue = require('./lib/steps/remove-from-queue')
 const logger = require('./lib/logger')
 
 logger('info', ['index', 'start'])
@@ -10,6 +12,8 @@ logger('info', ['index', 'start'])
 getNextJobFromQueue()
   .then(getFileData)
   .then(convertDocxToPdf)
+  .then(saveToDone)
+  .then(removeFromQueue)
   .then(() => {
     logger('info', ['index', 'done'])
     process.exit(0)
